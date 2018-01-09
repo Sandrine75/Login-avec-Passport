f372b1fcbc8c71a7168ceda77c560affca798933
Sandrine75 <sgautier75017@gmail.com>
2018-01-09 15:31:40 +0100

    part 2

--- routes/users.js
+++ routes/users.js
@@ -55,13 +55,4 @@ router.post('/register', function(req, res){
   
 });
 
-function ensureAuthenticated(req, res, next){
-	if(req.isAuthenticated()){
-		return next();
-	} else {
-		//req.flash('error_msg','You are not logged in');
-		res.redirect('/users/login');
-	}
-}
-
 module.exports = router;
